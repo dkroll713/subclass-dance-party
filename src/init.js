7c59ed50fb20427d5ba35cd756520a4e4e8819ab
Feifei Liang <mayliang021@gmail.com>
2022-06-06 20:15:40 -0500

    made a dance party

--- src/init.js
+++ src/init.js
@@ -16,18 +16,44 @@ $(document).ready(function() {
      * to the stage.
      */
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-
+    // console.log(this);
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
     // make a dancer with a random position
-
-    var dancer = dancerMakerFunction(
+    // console.log(dancerMakerFunctionName);
+    // console.log(dancerMakerFunction);
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+    console.log('dancer:', dancer);
+    $('body').append(dancer.$node);
+  });
+
+  $('.addPugButton').on('click', function(event) {
+    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+    var dancerMakerFunction = window[dancerMakerFunctionName];
+    console.log(dancerMakerFunction);
+
+    var dancer = new dancerMakerFunction(
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      Math.random() * 1000
+    );
+    console.log('dancer:', dancer);
     $('body').append(dancer.$node);
   });
 });
 
+$('.addSuperHerobutton').on('click', function(event) {
+  var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+  var dancerMakerFunction = window[dancerMakerFunctionName];
+  var dancer = new dancerMakerFunction(
+    $("body").height() * Math.random(),
+    $("body").width() * Math.random(),
+    Math.random() * 1000
+  );
+  $('body').append(dancer.$node);
+});
--- src/superHeroDancer.js
+++ src/superHeroDancer.js
@@ -1,27 +1,26 @@
-var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  console.log('top', top, '; left:', left, '; timeBetweenSteps:', timeBetweenSteps);
+var MakeSuperHearDancer = function(top, left, timeBetweenSteps) {
   MakeDancer.call(this, top, left, timeBetweenSteps);
-  console.log(this.$node);
-  this.$node = $('<span class="dancer" id="blinkyDancer"></span>');
+
   // var blinkyDancer = new MakeDancer(top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-
+  // this.$node = $(<span class="dancer" id="superHeroDancer"></span>);
+  this.$node = $('<span class="dancer" id="superHeroDancer" ><img src="../gif/batman.gif" class="animated-gif"></img></span>');
   this.oldStep = MakeDancer.prototype.step;
   this.oldStep();
-  // this.step();
   this.setPosition(top, left);
 };
 
-MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
-MakeBlinkyDancer.prototype.constructor = MakeBlinkyDancer;
+MakeSuperHearDancer.prototype = Object.create(MakeDancer.prototype);
+MakeSuperHearDancer.prototype.constructor = MakeSuperHearDancer;
 
-MakeBlinkyDancer.prototype.step = function() {
+MakeSuperHearDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
-  this.oldStep(this.timeBetweenSteps);
+  this.oldStep.call(this, this.timeBetweenSteps);
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
   this.$node.toggle();
 };
+
